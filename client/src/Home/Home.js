4fb4f41e03a39d3f5c590308bf8093a52bdcc48a
tespin <tristan.m.espinoza@gmail.com>
2022-12-14 07:39:28 -0800

    remove node modules, adding backend

--- client/src/Home/Home.js
+++ client/src/Home/Home.js
@@ -1,17 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import MainHeader from '../Header/MainHeader';
 import Card from '../UI/Card';
 import classes from './Home.module.css';
 import Usage from '../Usage/Usage';
 
 const Home = (props) => {
+    const [data, setData] = useState('');
+
+    useEffect( () => {
+        const fetchData = async () => {
+            const res = await fetch('/api');
+            const json = await res.json();
+            // return json;
+            setData(json.message);
+        }
+
+        fetchData()
+    })
+
     return (
         <Card className={classes.home}>
             <MainHeader />
             <main>
                 <div className={classes.about}>
                     <p>Banned Book API is a free JSON API for banned texts in the United States. The API uses PEN America’s Index of School Book Bans which collects “cases of book bans reported directly to PEN America and/or covered in the media” from June 2021 to July 2022.</p>
-                    <p>Intentions, goals, other context</p>
+                    {/* <p>Intentions, goals, other context</p> */}
+                    <p>{data ? data : 'Loading ...'}</p>
                 </div>
                 <Usage />
             </main>
---
+++ server/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+app.use(express.json({ limit: '1mb' }));
+app.use(express.urlencoded({ extended: true }));
+
+const port = process.env.PORT || 3001;
+
+app.get('/api', (req, res) => {
+    res.json({ message: "Hello from server!"});
+})
+
+app.listen(port, () => console.log(`listening on port ${port}`));
