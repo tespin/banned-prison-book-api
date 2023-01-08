5b9765d0af2806a10061fab42ea56d6a3c182faa
tespin <tristan.m.espinoza@gmail.com>
2023-01-08 13:35:40 -0800

    modifying path for static files

--- server/src/server.js
+++ server/src/server.js
@@ -6,11 +6,11 @@ const app = express();
 app.use(express.json({ limit: '1mb' }));
 app.use(express.urlencoded({ extended: true }));
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`listening on port ${port}`));
 
-app.use(express.static('../../client/build'));
+app.use(express.static(path.join(__dirname, '../../client/build')));
 app.use('/api/v1/books', v1Router);
-// app.listen(port, () => console.log(`listening on port ${port}`));
 
 // app.get('/api', (req, res) => {
 //     res.json({ message: "Hello from server!"});
