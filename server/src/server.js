9f3296d1597358992d8fa37e6b4e4936776dc47b
tespin <tristan.m.espinoza@gmail.com>
2023-01-08 13:30:45 -0800

    adding static serve to server file

--- server/src/server.js
+++ server/src/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const v1Router = require('./v1/routes/auth');
 
 const app = express();
@@ -7,8 +8,9 @@ app.use(express.urlencoded({ extended: true }));
 
 const port = process.env.PORT || 3001;
 
+app.use(express.static('../../client/build'));
 app.use('/api/v1/books', v1Router);
-app.listen(port, () => console.log(`listening on port ${port}`));
+// app.listen(port, () => console.log(`listening on port ${port}`));
 
 // app.get('/api', (req, res) => {
 //     res.json({ message: "Hello from server!"});
