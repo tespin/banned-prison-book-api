6be3fe6263b76d9818b107129e173fdd8fbcde9f
tespin <tristan.m.espinoza@gmail.com>
2023-01-08 12:26:46 -0800

    updating readme

---
+++ README.md
@@ -0,0 +1,30 @@
+A JSON API for books banned in prisons within the United States -- [https://banned-prison-books-api.onrender.com](https://banned-prison-books-api.onrender.com)
+
+## Usage
+
+### Publication
+Retrieve a banned book by its title.
+
+[https://banned-books-api.onrender.com/api/books?title=the%20autobiography%20of%20malcolm%20x](https://banned-books-api.onrender.com/api/books?title=the%20autobiography%20of%20malcolm%20x)
+
+### Author
+Retrieve banned books by a specified author.
+
+[https://banned-books-api.onrender.com/api/books?author=malcolm%20x](https://banned-books-api.onrender.com/api/books?author=malcolm%20x)
+
+### Year
+Retrieve books banned in a specific year.
+
+[https://banned-books-api.onrender.com/api/books?year=2015](https://banned-books-api.onrender.com/api/books?year=2015)
+
+### Reason
+Retrieve banned books according to the reason they were banned.
+
+[https://banned-books-api.onrender.com/api/books?reason=race](https://banned-books-api.onrender.com/api/books?reason=race)
+
+### State
+Retrieve all banned books in a specific state.
+
+[https://banned-books-api.onrender.com/api/books?title=the%wretched%of%the%earth](https://banned-books-api.onrender.com/api/books?state=az)
+
+## Licensing
--- client/src/Usage/Usage.js
+++ client/src/Usage/Usage.js
@@ -7,11 +7,11 @@ const Usage = (props) => {
         <>
             <div className={classes.usage}>
                 <h2>Usage</h2>
-                <Example heading='Publication' desc='Retrieve a banned book by its title.' endpoint='publication' params='the%wretched%of%the%earth'/>
-                <Example heading='Author' desc='Retrieve banned books by a specified author.' endpoint='author' params='frantz%fanon'/>
-                <Example heading='Year' desc='Retrieve books banned in a specific year.' endpoint='year' params='2016'/>
-                <Example heading='Reason' desc='Retrieve banned books according to the reason they were banned.' endpoint='author' params='race'/>
-                <Example heading='State' desc='Retrieve all banned books in a specific state.' endpoint='state_arc' params='ca'/>
+                <Example heading='Publication' desc='Retrieve a banned book by its title.' endpoint='publication' params='the%20autobiography%20of%20malcolm%20x'/>
+                <Example heading='Author' desc='Retrieve banned books by a specified author.' endpoint='author' params='malcolm%20x'/>
+                <Example heading='Year' desc='Retrieve books banned in a specific year.' endpoint='year' params='2015'/>
+                <Example heading='Reason' desc='Retrieve banned books according to the reason they were banned.' endpoint='reason' params='race'/>
+                <Example heading='State' desc='Retrieve all banned books in a specific state.' endpoint='state_arc' params='az'/>
                 {/* <Example heading='ISBN' desc='Retrieve a banned book by its ISBN.' endpoint='isbn' params='080214323'/>
                 <Example heading='State' desc='Retrieve all banned books in a specific state.' endpoint='state' params='california'/>
                 <Example heading='Genre' desc='Retrieve all banned books in a specific genre.' endpoint='genre' params='lgbtq'/>
