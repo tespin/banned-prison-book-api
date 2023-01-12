3a452798be3d22c7e4a9d635b8184d133507a341
tespin <tristan.m.espinoza@gmail.com>
2023-01-12 15:44:14 -0800

    adding redis cache

--- server/src/database/Book.js
+++ server/src/database/Book.js
@@ -1,42 +1,83 @@
 // const DB = require('./db.json');
 const bookDB = require('./db');
+const bookCache = require('./cache');
 
 const getAllBooks = async (filterParams) => {
     try {
-        let query = {};
+        let books;
+        
+        const cached = await bookCache.get('books');
+        console.log(typeof(cached));
+        if (cached) {
+            books = JSON.parse(cached);
+        } else {
+            books = await bookDB.find();
+            await bookCache.setex('books', 5, JSON.stringify(books));
+        }
 
         if (filterParams.publication) {
             const regex = new RegExp(filterParams.publication, 'i');
-            query.publication = { $regex: regex };
+            books = books.filter( (book) => book.publication.match(regex));
         }
 
         if (filterParams.author) {
             const regex = new RegExp(filterParams.author, 'i');
-            query.author = { $regex: regex };
+            books = books.filter( (book) => book.author.match(regex));
         }
 
         if (filterParams.year) {
-            query.year = { $eq: filterParams.year };
+            books = books.filter( (book) => book.year == filterParams.year);
         }
 
         if (filterParams.reason) {
             const regex = new RegExp(filterParams.reason, 'i');
-            query.reason = { $regex: regex };
+            books = books.filter( (book) => book.reason.match(regex));
         }
 
         if (filterParams.state_arc) {
             const regex = new RegExp(`^${filterParams.state_arc}$`);
-            query.state_arc = { $regex: regex };
+            books = books.filter( (book) => book.state_arc.match(regex));
         }
-
-        const books = await bookDB.find(query);
 
         if (filterParams.length) {
             books.splice(filterParams.length);
         }
-        
+
         // if no params specified, return all books
         return books;
+        
+        // let query = {};
+
+        // if (filterParams.publication) {
+        //     const regex = new RegExp(filterParams.publication, 'i');
+        //     query.publication = { $regex: regex };
+        // }
+
+        // if (filterParams.author) {
+        //     const regex = new RegExp(filterParams.author, 'i');
+        //     query.author = { $regex: regex };
+        // }
+
+        // if (filterParams.year) {
+        //     query.year = { $eq: filterParams.year };
+        // }
+
+        // if (filterParams.reason) {
+        //     const regex = new RegExp(filterParams.reason, 'i');
+        //     query.reason = { $regex: regex };
+        // }
+
+        // if (filterParams.state_arc) {
+        //     const regex = new RegExp(`^${filterParams.state_arc}$`);
+        //     query.state_arc = { $regex: regex };
+        // }
+
+        // if (filterParams.length) {
+        //     books.splice(filterParams.length);
+        // }
+
+        // if no params specified, return all books
+        // return books;
     } catch (error) {
         throw { status: 500, message: error };
     }
@@ -44,7 +85,18 @@ const getAllBooks = async (filterParams) => {
 
 const getRandomBook = async () => {
     // pick random book in database
-    const books = await bookDB.find();
+    // const books = await bookDB.find();
+
+    let books;
+        
+    const cached = await bookCache.get('books');
+    if (cached) {
+        books = JSON.parse(cached);
+    } else {
+        books = await bookDB.find();
+        await bookCache.setex('books', 5, JSON.stringify(books));
+    }
+
     const randomBook = books[Math.floor(Math.random() * books.length)]
     return randomBook;
 }
