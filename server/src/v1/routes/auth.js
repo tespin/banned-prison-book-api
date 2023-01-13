08eb34cd07354bd4267e6d7f5208883a45a1e858
tespin <tristan.m.espinoza@gmail.com>
2023-01-12 17:00:23 -0800

    clean up and removing redis for now

--- server/src/v1/routes/auth.js
+++ server/src/v1/routes/auth.js
@@ -4,8 +4,5 @@ const router = express.Router();
 
 router.get('/', bookController.getAllBooks);
 router.get('/random', bookController.getRandomBook);
-// router.post('/', (req, res) => {
-//     res.send('Create a new book');
-// })
 
 module.exports = router;
