11f2eff5cda067e336b0551d57ce1a780a46f62e
tespin <tristan.m.espinoza@gmail.com>
2023-01-27 06:38:09 -0800

    cleanup, and refactoring database layer

---
+++ server/src/database/utils.js
@@ -0,0 +1,22 @@
+const stateToAbbrev = {
+    'arizona': 'az',
+    'california': 'ca',
+    'connecticut': 'ct',
+    'florida': 'fl',
+    'georgia': 'ga',
+    'iowa': 'ia',
+    'illinois': 'il',
+    'kansas': 'ks',
+    'michigan': 'mi',
+    'montana': 'mt',
+    'north carolina': 'nc',
+    'new jersey': 'nj',
+    'oregon': 'or',
+    'rhode island': 'ri',
+    'south carolina': 'sc',
+    'texas': 'tx',
+    'virginia': 'va',
+    'wisconsin' : 'wi'
+};
+
+module.exports = { stateToAbbrev };
