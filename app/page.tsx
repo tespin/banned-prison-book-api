81e4eb02ab074346457b520cd9edc041dcb31cf1
Tristan Espinoza <tristan.m.espinoza@gmail.com>
2023-08-10 22:45:15 -0700

    feat: interactivity added to search bar

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 const Page = () => {
   return (
-    <h1 className=''>Banned Prison Books is a tool for identifying books that have been banned in state prison systems.</h1>
+    <h1 className='mt-16'>Banned Prison Books is a tool for identifying books that have been banned in state prison systems.</h1>
   )
 }
 
--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,10 +1,20 @@
+'use client'
+
+import { useState } from 'react';
 import Flex from '@/components/Flex';
 
 const SearchBar = () => {
+  const [input, setInput] = useState('');
+
+  const inputHandler = (e) => {
+    console.log(e.target.value);
+    setInput(e.target.value);
+  }
+
   return (
-    <Flex className='xs:justify-between text-base'>
+    <Flex className='xs:justify-between xs:items-center mt-16 text-base'>
       <label className='basis-2/3' htmlFor='search'>I'm looking for books in ...</label>
-      <input className='border grow min-w-0' type='text' placeholder='State' id='search' />
+      <input className='px-2 py-1 border min-w-0' onChange={inputHandler} type='text' placeholder='State' id='search' />
     </Flex>
   )
 }
