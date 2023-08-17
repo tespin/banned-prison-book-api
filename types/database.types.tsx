4a42e949194d49d7776bf885c7a999071cfc828c
Tristan Espinoza <tristan.m.espinoza@gmail.com>
2023-08-16 20:39:39 -0700

    feat: adding supabase

---
+++ types/database.types.tsx
@@ -0,0 +1,55 @@
+export type Json =
+  | string
+  | number
+  | boolean
+  | null
+  | { [key: string]: Json | undefined }
+  | Json[]
+
+export interface Database {
+  public: {
+    Tables: {
+      books: {
+        Row: {
+          author: string | null
+          date: string | null
+          id: string
+          publication: string | null
+          reason: string | null
+          state_arc: string | null
+        }
+        Insert: {
+          author?: string | null
+          date?: string | null
+          id?: string
+          publication?: string | null
+          reason?: string | null
+          state_arc?: string | null
+        }
+        Update: {
+          author?: string | null
+          date?: string | null
+          id?: string
+          publication?: string | null
+          reason?: string | null
+          state_arc?: string | null
+        }
+        Relationships: []
+      }
+    }
+    Views: {
+      [_ in never]: never
+    }
+    Functions: {
+      [_ in never]: never
+    }
+    Enums: {
+      [_ in never]: never
+    }
+    CompositeTypes: {
+      [_ in never]: never
+    }
+  }
+}
+
+export type Tables<T extends keyof Database['public']['Tables']> = Database['public']['Tables'][T]['Row'];
--- utils/utils.tsx
+++ utils/utils.tsx
@@ -1,6 +1,27 @@
+const stateToAbbrev = {
+  'arizona': 'az',
+  'california': 'ca',
+  'connecticut': 'ct',
+  'florida': 'fl',
+  'georgia': 'ga',
+  'iowa': 'ia',
+  'illinois': 'il',
+  'kansas': 'ks',
+  'michigan': 'mi',
+  'montana': 'mt',
+  'north carolina': 'nc',
+  'new jersey': 'nj',
+  'oregon': 'or',
+  'rhode island': 'ri',
+  'south carolina': 'sc',
+  'texas': 'tx',
+  'virginia': 'va',
+  'wisconsin' : 'wi'
+};
+
 const getFiltered = (data, input) => {
   if (input.length < 1) return [];
   else return data.filter((item) => item.name.toLowerCase().includes(input.toLowerCase()));
 }
 
-export { getFiltered };
+export { getFiltered, stateToAbbrev };
