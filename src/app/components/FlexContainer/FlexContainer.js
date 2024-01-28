e2e065230bb2db7315ce78e70355fc6aafea5f64
Tristan Espinoza <tristan.m.espinoza@gmail.com>
2024-01-28 14:10:42 -0800

    feat: added flexbox component

---
+++ src/app/components/FlexContainer/FlexContainer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function FlexContainer({ children, className }) {
+  return (
+    <div className={`flex ${className ? className : undefined}`}>
+      {children}
+    </div>
+  )
+}
