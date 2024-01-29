c2489de354911ad3189f3d477bc2f4b401e4c7ec
Tristan Espinoza <tristan.m.espinoza@gmail.com>
2024-01-29 14:48:11 -0800

    feat: added search bar

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React from "react";
+import FlexContainer from "./components/FlexContainer";
+import SearchBar from "./components/SearchBar";
 
 function Home() {
   return (
     <main>
-      <h1 className='mt-16'>Banned Prison Books is a tool for identifying books that have been banned in state prison systems.</h1>
-      <p>Search for books below. Alternatively, explore the API.</p>
+      <FlexContainer className="xs:flex-col xs:max-w-sm">
+        <div className="text-center">
+          <h1 className="mt-16 text-4xl">
+            Banned Prison Books is a tool for finding books that have been
+            banned in state prison systems.
+          </h1>
+          <p className="opacity-50 mt-4">
+            Search for books below. Or, explore the API.
+          </p>
+        </div>
+        <SearchBar className="mt-10" />
+      </FlexContainer>
     </main>
   );
 }
