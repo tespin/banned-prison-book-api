90f293609678453b4a9358a2f06f510aaea067aa
Tristan Espinoza <tristan.m.espinoza@gmail.com>
2024-01-31 17:30:20 -0800

    feat: adding autocomplete component

---
+++ src/app/components/AutocompleteResults/AutocompleteResults.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import States from "../../utils/states.json";
+
+function AutocompleteResults() {
+  return (
+    <div className="absolute top-full p-0">
+      <div>Alabama</div>
+      <div>California</div>
+      <div>New Jersey</div>
+      <div>Rhdoe Island</div>
+    </div>
+  );
+}
+
+export default AutocompleteResults;
