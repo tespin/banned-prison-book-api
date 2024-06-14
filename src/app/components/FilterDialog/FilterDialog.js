180300d7e718b86b9b2e33373300445de31d12ff
Tristan Espinoza <tristan.m.espinoza@gmail.com>
2024-06-14 14:06:53 -0700

    feat: addign filter dialog

---
+++ src/app/components/FilterDialog/FilterDialog.js
@@ -0,0 +1,26 @@
+"use client";
+
+import React, { useState } from "react";
+import * as Dialog from "@radix-ui/react-dialog";
+import FilterButton from "../FilterButton";
+
+function FilterDialog() {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <Dialog.Root open={open} onOpenChange={setOpen}>
+      <Dialog.Trigger asChild>
+        <FilterButton />
+      </Dialog.Trigger>
+      <Dialog.Portal>
+        <Dialog.Overlay />
+        <Dialog.Content>
+          <p>what's up</p>
+          <Dialog.Close>X</Dialog.Close>
+        </Dialog.Content>
+      </Dialog.Portal>
+    </Dialog.Root>
+  );
+}
+
+export default FilterDialog;
